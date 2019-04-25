aaae6540b98f5765aaefe3e1f836a50d43e12a72
Kazumasa Shimomura <shimomura_kazumasa@cyberagent.co.jp>
2019-04-25 17:23:29 +0900

    Initial Commit

---
+++ FlexibleInputStackBar/FlexibleInputStackBar.h
@@ -0,0 +1,19 @@
+//
+//  FlexibleInputStackBar.h
+//  FlexibleInputStackBar
+//
+//  Created by 下村 一将 on 2019/04/25.
+//  Copyright © 2019 kazuringo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlexibleInputStackBar.
+FOUNDATION_EXPORT double FlexibleInputStackBarVersionNumber;
+
+//! Project version string for FlexibleInputStackBar.
+FOUNDATION_EXPORT const unsigned char FlexibleInputStackBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlexibleInputStackBar/PublicHeader.h>
+
+
